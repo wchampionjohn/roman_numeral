9b990998704de9e4d27a38429536e57fba9e5e53
john.hsieh <john.hsieh@rockmobile.com>
2016-10-27 11:43:55 +0800

    [add]新增並通過輸入值9 10 40 50的test case

--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -5,14 +5,18 @@ require_relative '../lib/roman_numerals'
 describe 'Roman Numeral Converter' do
   context '#conver' do
     [
-      [1, 'I'],
-      [2, 'II'],
-      [3, 'III'],
-      [4, 'IV'],
-      [5, 'V'],
-      [6, 'VI'],
-      [7, 'VII'],
-      [8, 'VIII'],
+      [1,  'I'],
+      [2,  'II'],
+      [3,  'III'],
+      [4,  'IV'],
+      [5,  'V'],
+      [6,  'VI'],
+      [7,  'VII'],
+      [8,  'VIII'],
+      [9,  'IX'],
+      [10, 'X'],
+      [40, 'XL'],
+      [50, 'L'],
     ].each do |numeric_pair|
       it "should return #{numeric_pair.last} if convert number of #{numeric_pair.first}" do
         roman = RomanNumerals.new
