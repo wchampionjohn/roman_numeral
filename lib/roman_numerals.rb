abf2badf1b1888b8197560459e4985823b9833e3
john.hsieh <john.hsieh@rockmobile.com>
2016-10-27 10:20:29 +0800

    [add]通過輸入值1的test case

---
+++ lib/roman_numerals.rb
@@ -0,0 +1,5 @@
+class RomanNumerals
+  def convert num
+    'I'
+  end
+end
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -1,9 +1,10 @@
 require 'rspec'
 require 'byebug'
+require_relative '../lib/roman_numerals'
 
 describe 'Roman Numeral Converter' do
   it "should return I if convert  number of 1" do
-    roman = RomanNumeral.new
+    roman = RomanNumerals.new
     expect(roman.convert(1)).to eq 'I'
   end
 end
