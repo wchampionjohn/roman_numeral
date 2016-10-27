0937eeb08031095a9d7c47653f3bfc7c961d72b5
john.hsieh <john.hsieh@rockmobile.com>
2016-10-27 10:34:26 +0800

    [add]新增輸入值2 3的test case

--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -3,8 +3,20 @@ require 'byebug'
 require_relative '../lib/roman_numerals'
 
 describe 'Roman Numeral Converter' do
-  it "should return I if convert  number of 1" do
-    roman = RomanNumerals.new
-    expect(roman.convert(1)).to eq 'I'
+  context '#conver' do
+    it "should return I if convert number of 1" do
+      roman = RomanNumerals.new
+      expect(roman.convert(1)).to eq 'I'
+    end
+
+    it "should return II if convert  number of 2" do
+      roman = RomanNumerals.new
+      expect(roman.convert(2)).to eq 'II'
+    end
+
+    it "should return III if convert number of 3" do
+      roman = RomanNumerals.new
+      expect(roman.convert(3)).to eq 'III'
+    end
   end
 end
