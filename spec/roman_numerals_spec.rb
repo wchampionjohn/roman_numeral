21300bd599347dbdafe224814ab833b6b85f084c
john.hsieh <john.hsieh@rockmobile.com>
2016-10-27 10:17:05 +0800

    [add] first test case

--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -2,4 +2,8 @@ require 'rspec'
 require 'byebug'
 
 describe 'Roman Numeral Converter' do
+  it "should return I if convert  number of 1" do
+    roman = RomanNumeral.new
+    expect(roman.convert(1)).to eq 'I'
+  end
 end
